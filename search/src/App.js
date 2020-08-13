4f6772724aa13f805f85439bdfc3540fdaa387ea
Joel Nothman <joel.nothman@gmail.com>
2020-08-13 16:29:35 +1000

    Attempt to implement reactivesearch quickstart

--- search/src/App.js
+++ search/src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { 
+	ReactiveBase, CategorySearch, SingleRange, ResultCard, ReactiveList 
+} from '@appbaseio/reactivesearch';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+				<ReactiveBase
+				app="kibana_sample_data_ecommerce"
+				url="http://localhost:9200/">
+			    <CategorySearch
+						componentId="searchbox"
+						dataField="day_of_week"
+						categoryField="day_of_week"
+						placeholder="Search for days"
+					/>
+					<ReactiveList
+						componentId="result"
+						title="Results"
+						dataField="day_of_week"
+						from={0}
+						size={5}
+						pagination={true}
+						react={{
+							and: ["searchbox" /*, "ratingsfilter" */ ]
+						}}
+						render={({ data }) => (
+							<ReactiveList.ResultCardsWrapper>
+								{
+									data.map(item => (
+										<ResultCard key={item._id}>
+											<ResultCard.Image 
+												src='https://bit.do/demoimg'
+											/>
+											<ResultCard.Title 
+												dangerouslySetInnerHTML={{ 
+													__html: item.customer_phone
+												}} 
+											/>
+											<ResultCard.Description>
+												{item.day_of_week + " " + '*'.repeat(item.total_quantity)}
+											</ResultCard.Description>
+										</ResultCard>
+									))
+								}
+							</ReactiveList.ResultCardsWrapper>
+						)}
+					/>
+				</ReactiveBase>
+		);
+	}
 }
 
 export default App;
