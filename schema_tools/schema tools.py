18d58528a3317c436925a05374e342437b3e73bd
Henry Lydecker <50643975+hlydecker@users.noreply.github.com>
2020-08-27 19:35:01 +1000

    Update schema tools.py

--- schema_tools/schema tools.py
+++ schema_tools/schema tools.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
 import yaml
 import textwrap
 schema = yaml.safe_load(open('AgContext.yaml'))
@@ -14,16 +8,3 @@ for name, prop in schema['properties'].items():
     print(textwrap.indent(yaml.dump(prop), '# '))
     print(f"{name}: <TO DO: insert value here{' or remove this line' if name not in required else ''}>")
     print()
-
-
-# In[ ]:
-
-
-get_ipython().system('jupyter nbconvert --to script *.ipynb')
-
-
-# In[ ]:
-
-
-
-
