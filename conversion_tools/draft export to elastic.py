dfd189fba769cb7b410e3ec13481ac0fa1cb5846
Joel Nothman <joel.nothman@gmail.com>
2020-08-28 11:01:06 +1000

    Show test image thumbnails in search results (#40)
    * Show test image thumbnails in search results
    
    Fixes #39
    
    Includes some small fixes to conversion tools (and a seeming reversion of width and height calculation in deepweeds JSON data).
    
    Includes scripts to index test data and to download & downsize images for thumbnail display.
    
    * removed conflicting files
    
    Master has different versions of these that have solved these issues.
    
    Co-authored-by: Henry Lydecker <henry.lydecker@sydney.edu.au>

---
+++ conversion_tools/draft export to elastic.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Exports Weed COCO on stdin to ElasticSearch _bulk data on stdout
+
+import sys
+import json
+
+
+coco = json.load(sys.stdin)
+del coco["info"]
+try:
+    del coco["collection_memberships"]
+except KeyError:
+    pass
+
+
+id_lookup = {}
+for key, objs in coco.items():
+    for obj in objs:
+        id_lookup[key, obj["id"]] = obj
+
+
+for annotation in coco["annotations"]:
+    image = id_lookup["images", annotation["image_id"]]
+    image.setdefault("annotations", []).append(annotation)
+    annotation["category"] = id_lookup["categories", annotation["category_id"]]
+    if "agcontext_id" in annotation:
+        # Handle legacy version where agcontext_id appears in annotation
+        annotation["agcontext"] = id_lookup["agcontexts", annotation["agcontext_id"]]
+    else:
+        annotation["agcontext"] = id_lookup["agcontexts", image["agcontext_id"]]
+    # todo: add collection, license
+
+
+f = sys.stdout
+f.write("\n")
+for image in coco["images"]:
+    json.dump({"index": {"_index": "weedid", "_type": "image"}}, f)
+    f.write("\n")
+    json.dump(image, f)
+    f.write("\n")
