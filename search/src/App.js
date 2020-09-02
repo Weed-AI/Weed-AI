f99e427edc12c0e28d2cb2cba36eaa22496cfe60
Henry Lydecker <henry.lydecker@sydney.edu.au>
2020-09-02 14:51:08 +1000

    super basic widget functionality

--- search/src/App.js
+++ search/src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import {
-	ReactiveBase, CategorySearch, SingleRange, ResultCard, ReactiveList
+	ReactiveBase,
+	CategorySearch,
+	DataSearch,
+	SingleRange,
+	ResultCard,
+	MultiList,
+	ReactiveList
 } from '@appbaseio/reactivesearch';
 import logo from './logo.svg';
 import './App.css';
@@ -8,48 +14,74 @@ import './App.css';
 class App extends Component {
 	render() {
 		return (
-				<ReactiveBase
-				app="weedid"
-				url="http://localhost:9200/">
+			<ReactiveBase
+			app="weedid"
+			url="http://localhost:9200/">
+				<div style={{ display: "flex", flexDirection: "row" }}>
+					<div style={{ display: "flex", flexDirection: "column", width: "40%" }}>
 			    <CategorySearch
 						componentId="searchbox"
-						dataField="day_of_week"
-						categoryField="day_of_week"
-						placeholder="Search for days"
-					/>
-					<ReactiveList
-						componentId="result"
-						title="Results"
-						dataField="day_of_week"
-						from={0}
-						size={5}
-						pagination={true}
+						dataField="annotations.category.common_name.keyword"
+						categoryField="common_name"
+						placeholder="Search for species"
+				/>
+				<MultiList
+						componentId="rolefilter"
+						title="Filter by role"
+						dataField="annotations.category.role.keyword"
+						sortBy="asc"
+						queryFormat="or"
+						selectAllLabel="All roles"
+						showCheckbox={true}
+						showCount={true}
+						showSearch={true}
+						placeholder="Search Role"
 						react={{
-							and: ["searchbox" /*, "ratingsfilter" */ ]
+							and: ["CategoryFilter", "SearchFilter"]
 						}}
-						render={({ data }) => (
-							<ReactiveList.ResultCardsWrapper>
-								{
-									data.map(item => (
-										<ResultCard key={item._id}>
-											<ResultCard.Image
-												src={'thumbnails/' + item.file_name}
-											/>
-											<ResultCard.Title
-												dangerouslySetInnerHTML={{
-													__html: item.customer_phone
-												}}
-											/>
-											<ResultCard.Description>
-												{item.day_of_week + " " + '*'.repeat(item.total_quantity)}
-											</ResultCard.Description>
-										</ResultCard>
-									))
-								}
-							</ReactiveList.ResultCardsWrapper>
-						)}
+						showFilter={true}
+						filterLabel="Role"
+						URLParams={false}
+						style={{
+							padding: "5px",
+							marginTop: "10px"
+						}}
 					/>
-				</ReactiveBase>
+				</div>
+				<ReactiveList
+					componentId="result"
+					title="Results"
+					dataField="day_of_week"
+					from={0}
+					size={5}
+					pagination={true}
+					react={{
+						and: ["searchbox" /*, "ratingsfilter" */ ]
+					}}
+					render={({ data }) => (
+						<ReactiveList.ResultCardsWrapper>
+							{
+								data.map(item => (
+									<ResultCard key={item._id}>
+										<ResultCard.Image
+											src={'thumbnails/' + item.file_name}
+										/>
+										<ResultCard.Title
+											dangerouslySetInnerHTML={{
+												__html: item.customer_phone
+											}}
+										/>
+										<ResultCard.Description>
+											{item.day_of_week + " " + '*'.repeat(item.total_quantity)}
+										</ResultCard.Description>
+									</ResultCard>
+								))
+							}
+						</ReactiveList.ResultCardsWrapper>
+					)}
+				/>
+				</div>
+			</ReactiveBase>
 		);
 	}
 }
