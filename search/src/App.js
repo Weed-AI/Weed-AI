baa6715e6de236040ed5bd6cb4b18e05216827bc
Henry Lydecker <henry.lydecker@gmail.com>
2020-09-17 17:20:56 +1000

    Refactor search widget attributes with spread attributes (#75)
    * Refactor search widget attributes with spread attributes
    
    * further refactor of search widgets
    
    * Update App.js
    
    Co-authored-by: Joel Nothman <joel.nothman@gmail.com>
    Co-authored-by: Henry Lydecker <henry.lydecker@sydney.edu.au>

--- search/src/App.js
+++ search/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {
 	ReactiveBase,
-	DataSearch,
 	RangeSlider,
 	ResultCard,
 	MultiList,
@@ -13,6 +12,23 @@ import './App.css';
 
 class App extends Component {
 	render() {
+		let facetProps = {
+			queryFormat: "and",
+			URLParams: true,
+			react: {
+				and: ["searchbox", "resslider", "agcontextfilter", "rolefilter", "speciesfilter", "grainstextfilter"]
+			}
+		}
+		let multilistFacetProps = {
+			showCheckbox: true,
+			showCount: true,
+			showSearch: true,
+			showFilter: true,
+			style: {
+				padding: "5px",
+				marginTop: "10px"
+			},
+		}
 		return (
 			<ReactiveBase
 				app="weedid"
@@ -31,114 +47,49 @@ class App extends Component {
 				}}
 			>
 				<div style={{ position: "fixed", width: "20rem", overflow: "scroll", height: "100%" }}>
-					{/* <DataSearch
-						componentId="searchbox"
-						dataField={["annotations__category__common_name","annotation__category__role.keyword","agcontext__agcontext_name.keyword","annotation__category__species.keyword"]}
-						placeholder="Search for species, role, or agcontext"
-						autoSuggest={true}
-						defaultSuggestions={[
-							{label: "Weed", value: "weed"},
-							{label: "Carrot", value: "carrot"}
-						]}
-						highlight={true}
-						highlightField={["annotations__category__common_name","annotation__category__role.keyword","agcontext__agcontext_name.keyword","annotation__category__species.keyword"]}
-						queryFormat="or"
-						fuzziness={0}
-						debounce={100}
-						react={{
-						  and: ["searchbox","resslider","agcontextfilter","rolefilter","speciesfilter","grainstextfilter"]
-						}}
-						showFilter={true}
-						filterLabel="General filter"
-						URLParams={true}
-				/> */}
 					<MultiList
 						componentId="rolefilter"
 						title="Filter by role"
 						dataField="annotation__category__role.keyword"
 						sortBy="asc"
-						queryFormat="and"
 						selectAllLabel="All roles"
-						showCheckbox={true}
-						showCount={true}
-						showSearch={true}
 						placeholder="Search Role"
-						react={{
-							and: ["searchbox", "resslider", "agcontextfilter", "rolefilter", "speciesfilter", "grainstextfilter"]
-						}}
-						showFilter={true}
 						filterLabel="Role"
-						URLParams={true}
-						style={{
-							padding: "5px",
-							marginTop: "10px"
-						}}
+						{...facetProps}
+						{...multilistFacetProps}
 					/>
 					<MultiList
 						componentId="speciesfilter"
 						title="Filter by species"
 						dataField="annotation__category__species.keyword"
 						sortBy="asc"
-						queryFormat="and"
 						selectAllLabel="All species"
-						showCheckbox={true}
-						showCount={true}
-						showSearch={true}
 						placeholder="Search Species"
-						react={{
-							and: ["searchbox", "resslider", "agcontextfilter", "rolefilter", "speciesfilter", "grainstextfilter"]
-						}}
-						showFilter={true}
 						filterLabel="Species"
-						URLParams={true}
-						style={{
-							padding: "5px",
-							marginTop: "10px"
-						}}
+						{...facetProps}
+						{...multilistFacetProps}
 					/>
 					<MultiList
 						componentId="agcontextfilter"
 						title="Filter by Collection"
 						dataField="agcontext__agcontext_name.keyword"
 						sortBy="asc"
-						queryFormat="and"
 						selectAllLabel="All collections"
-						showCheckbox={true}
-						showCount={true}
-						showSearch={true}
 						placeholder="Search collection"
-						react={{
-							and: ["searchbox", "resslider", "agcontextfilter", "rolefilter", "speciesfilter", "grainstextfilter"]
-						}}
-						showFilter={true}
 						filterLabel="Agcontext"
-						URLParams={true}
-						style={{
-							padding: "5px",
-							marginTop: "10px"
-						}}
+						{...facetProps}
+						{...multilistFacetProps}
 					/>
 					<MultiList
 						componentId="grainstextfilter"
 						title="Filter by Growth Stage"
 						dataField="agcontext__grains_descriptive_text.keyword"
 						sortBy="asc"
-						queryFormat="and"
 						selectAllLabel="All growth stages"
-						showCheckbox={true}
-						showCount={true}
-						showSearch={true}
 						placeholder="Search growth stage"
-						react={{
-							and: ["searchbox", "resslider", "agcontextfilter", "rolefilter", "speciesfilter", "grainstextfilter"]
-						}}
-						showFilter={true}
 						filterLabel="Growth stage"
-						URLParams={true}
-						style={{
-							padding: "5px",
-							marginTop: "10px"
-						}}
+						{...facetProps}
+						{...multilistFacetProps}
 					/>
 					<RangeSlider
 						componentId="resslider"
@@ -154,11 +105,9 @@ class App extends Component {
 						}}
 						stepValue={10000}
 						showHistogram={true}
-						showFilter={true}
 						interval={10000}
-						react={{
-							and: ["searchbox", "resslider", "agcontextfilter", "rolefilter", "speciesfilter", "grainstextfilter"]
-						}}
+						{...facetProps}
+						{...multilistFacetProps}
 					/>
 				</div>
 				<div style={{ position: "absolute", left: "20rem" }}>
@@ -169,6 +118,7 @@ class App extends Component {
 						dataField="annotations__category__common_name"
 						from={0}
 						size={20}
+						{...facetProps}
 						infiniteScroll={true}
 						react={{
 							and: ["searchbox", "resslider", "agcontextfilter", "rolefilter", "speciesfilter", "grainstextfilter"]
@@ -187,7 +137,7 @@ class App extends Component {
 												}}
 											/>
 											<ResultCard.Description>
-												{item._id + " " + '*'.repeat(item.total_quantity)}
+												{"Crop: " + item.agcontext__crop_type}
 											</ResultCard.Description>
 										</ResultCard>
 									))
