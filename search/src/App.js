619ce1fd1108f75456b2fd12ed8a4ed2800c61f3
Henry Lydecker <henry.lydecker@gmail.com>
2020-09-17 17:23:52 +1000

    Refactor props (#78)
    * use and for filtering
    
    filter/facet now work use and rather than or
    
    * url reflecting search params
    
    * added grains text filter
    
    * restored deleted line comment
    
    * schem spatial fix
    
    * minor schema style fix
    
    * yaml fix to prevent validation fail
    
    * Update AgContext.yaml
    
    * Update AgContext.yaml
    
    * Update AgContext.yaml
    
    * Update AgContext.yaml
    
    yet another attempt to sort this out
    
    * datum string test patch
    
    * schema enum patch
    
    * schema validation fix attempt
    
    * Update AgContext.yaml
    
    * Update AgContext.yaml
    
    * Update cwfid_to_json.py
    
    * Update cwfid_to_json.py
    
    * Update AgContext.yaml
    
    * Small tweaks to display (#66)
    
    Co-authored-by: Joel Nothman <joel.nothman@gmail.com>
    
    * removed searchbox, added selectedfilters
    
    - removed generic searcbox; still unsure what to do with it.
    
    - added SelectedFilters results UI element. It rocks.
    
    * Refactor search widget attributes with spread attributes
    
    * Update search/src/App.js
    
    Co-authored-by: Joel Nothman <joel.nothman@gmail.com>
    
    * further refactor of search widgets
    
    * Update App.js
    
    Co-authored-by: Henry Lydecker <henry.lydecker@sydney.edu.au>
    Co-authored-by: Joel Nothman <joel.nothman@gmail.com>

--- search/src/App.js
+++ search/src/App.js
@@ -1,129 +1,103 @@
 import React, { Component } from 'react';
 import {
 	ReactiveBase,
-	CategorySearch,
 	RangeSlider,
-	ReactiveOpenStreetMap,
 	ResultCard,
 	MultiList,
-	ReactiveList
+	ReactiveList,
+	SelectedFilters
 } from '@appbaseio/reactivesearch';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 	render() {
+		let facetProps = {
+			queryFormat: "and",
+			URLParams: true,
+			react: {
+				and: ["searchbox", "resslider", "agcontextfilter", "rolefilter", "speciesfilter", "grainstextfilter"]
+			}
+		}
+		let multilistFacetProps = {
+			showCheckbox: true,
+			showCount: true,
+			showSearch: true,
+			showFilter: true,
+			style: {
+				padding: "5px",
+				marginTop: "10px"
+			},
+		}
 		return (
 			<ReactiveBase
-			app="weedid"
-			url="http://localhost:9200/"
-			theme={{
-				typography: {
-				  fontFamily: 'Raleway, Helvetica, sans-serif',
-				},
-				colors: {
-				  primaryColor: '#0A0A0A',
-				  titleColor: '#E64626',
-				},
-				component: {
-				  padding: 10
-				}
-			  }}
+				app="weedid"
+				url="http://localhost:9200/"
+				theme={{
+					typography: {
+						fontFamily: 'Raleway, Helvetica, sans-serif',
+					},
+					colors: {
+						primaryColor: '#0A0A0A',
+						titleColor: '#E64626',
+					},
+					component: {
+						padding: 10
+					}
+				}}
 			>
-			<div style={{ display: "flex", flexDirection: "row" }}>
-			<div style={{ display: "flex", flexDirection: "column", width: "40%" }}>
-			    <CategorySearch
-						componentId="searchbox"
-						dataField={["annotations.category.common_name.keyword","annotations.category.role.keyword","annotations.agcontext.agcontext_name.keyword","annotations.category.species.keyword"]}
-						categoryField={["annotations.category.common_name.keyword","annotations.category.role.keyword","annotations.agcontext.agcontext_name.keyword","annotations.category.species.keyword"]}
-						placeholder="Search for species, role, or agcontext"
-						autoSuggest={true}
-						defaultSuggestions={[{label: "Weed", value: "weed"}]}
-						highlight={false}
-						highlightField={["annotations.category.common_name.keyword","annotations.category.role.keyword","annotations.agcontext.agcontext_name.keyword","annotations.category.species.keyword"]}
-						queryFormat="or"
-						fuzziness={0}
-						debounce={100}
-						react={{
-						  and: ["CategoryFilter", "SearchFilter"]
-						}}
-						showFilter={true}
-						filterLabel="Venue filter"
-						URLParams={false}
-				/>
-				<MultiList
+				<div style={{ position: "fixed", width: "20rem", overflow: "scroll", height: "100%" }}>
+					<MultiList
 						componentId="rolefilter"
 						title="Filter by role"
-						dataField="annotations.category.role.keyword"
+						dataField="annotation__category__role.keyword"
 						sortBy="asc"
-						queryFormat="or"
 						selectAllLabel="All roles"
-						showCheckbox={true}
-						showCount={true}
-						showSearch={true}
 						placeholder="Search Role"
-						react={{
-							and: ["searchbox", "SearchFilter"]
-						}}
-						showFilter={true}
 						filterLabel="Role"
-						URLParams={false}
-						style={{
-							padding: "5px",
-							marginTop: "10px"
-						}}
+						{...facetProps}
+						{...multilistFacetProps}
 					/>
-				<MultiList
+					<MultiList
 						componentId="speciesfilter"
 						title="Filter by species"
-						dataField="annotations.category.species.keyword"
+						dataField="annotation__category__species.keyword"
 						sortBy="asc"
-						queryFormat="or"
 						selectAllLabel="All species"
-						showCheckbox={true}
-						showCount={true}
-						showSearch={true}
 						placeholder="Search Species"
-						react={{
-							and: ["searchbox", "SearchFilter"]
-						}}
-						showFilter={true}
 						filterLabel="Species"
-						URLParams={false}
-						style={{
-							padding: "5px",
-							marginTop: "10px"
-						}}
+						{...facetProps}
+						{...multilistFacetProps}
 					/>
-				<MultiList
+					<MultiList
 						componentId="agcontextfilter"
-						title="Filter by agcontext"
-						dataField="annotations.agcontext.agcontext_name.keyword"
+						title="Filter by Collection"
+						dataField="agcontext__agcontext_name.keyword"
 						sortBy="asc"
-						queryFormat="or"
-						selectAllLabel="All agcontexts"
-						showCheckbox={true}
-						showCount={true}
-						showSearch={true}
-						placeholder="Search Agcontext"
-						react={{
-							and: ["searchbox", "SearchFilter"]
-						}}
-						showFilter={true}
+						selectAllLabel="All collections"
+						placeholder="Search collection"
 						filterLabel="Agcontext"
-						URLParams={false}
-						style={{
-							padding: "5px",
-							marginTop: "10px"
-						}}
+						{...facetProps}
+						{...multilistFacetProps}
 					/>
-				<RangeSlider
+					<MultiList
+						componentId="grainstextfilter"
+						title="Filter by Growth Stage"
+						dataField="agcontext__grains_descriptive_text.keyword"
+						sortBy="asc"
+						selectAllLabel="All growth stages"
+						placeholder="Search growth stage"
+						filterLabel="Growth stage"
+						{...facetProps}
+						{...multilistFacetProps}
+					/>
+					<RangeSlider
 						componentId="resslider"
 						dataField="resolution"
 						title="Resolution (pixels)"
 						range={{
 							"start": 0,
-							"end": 100000
+							"end": 1500000
 						}}
 						rangeLabels={{
 							"start": "Start",
@@ -131,45 +105,46 @@ class App extends Component {
 						}}
 						stepValue={10000}
 						showHistogram={true}
-						showFilter={true}
 						interval={10000}
-						react={{
-							and: ["searchbox","CategoryFilter", "SearchFilter"]
-						}}
-				/>
+						{...facetProps}
+						{...multilistFacetProps}
+					/>
 				</div>
-				<ReactiveList
-					componentId="result"
-					title="Results"
-					dataField="annotations.category.common_name.keyword"
-					from={0}
-					size={20}
-					pagination={true}
-					react={{
-						and: ["searchbox","resslider","agcontextfilter","rolefilter","speciesfilter"]
-					}}
-					render={({ data }) => (
-						<ReactiveList.ResultCardsWrapper>
-							{
-								data.map(item => (
-									<ResultCard key={item._id}>
-										<ResultCard.Image
-											src={'thumbnails/' + item.file_name}
-										/>
-										<ResultCard.Title
-											dangerouslySetInnerHTML={{
-												__html: item.customer_phone
-											}}
-										/>
-										<ResultCard.Description>
-											{item._id + " " + '*'.repeat(item.total_quantity)}
-										</ResultCard.Description>
-									</ResultCard>
-								))
-							}
-						</ReactiveList.ResultCardsWrapper>
-					)}
-				/>
+				<div style={{ position: "absolute", left: "20rem" }}>
+					<SelectedFilters />
+					<ReactiveList
+						componentId="result"
+						title="Results"
+						dataField="annotations__category__common_name"
+						from={0}
+						size={20}
+						{...facetProps}
+						infiniteScroll={true}
+						react={{
+							and: ["searchbox", "resslider", "agcontextfilter", "rolefilter", "speciesfilter", "grainstextfilter"]
+						}}
+						render={({ data }) => (
+							<ReactiveList.ResultCardsWrapper>
+								{
+									data.map(item => (
+										<ResultCard key={item._id}>
+											<ResultCard.Image
+												src={'thumbnails/' + item.thumbnail}
+											/>
+											<ResultCard.Title
+												dangerouslySetInnerHTML={{
+													__html: item.customer_phone
+												}}
+											/>
+											<ResultCard.Description>
+												{"Crop: " + item.agcontext__crop_type}
+											</ResultCard.Description>
+										</ResultCard>
+									))
+								}
+							</ReactiveList.ResultCardsWrapper>
+						)}
+					/>
 				</div>
 			</ReactiveBase>
 		);
