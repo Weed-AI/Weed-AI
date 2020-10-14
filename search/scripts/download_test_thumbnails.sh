7a08c39c6091318318f451fe4fc19eaf6c9aa792
Joel Nothman <joel.nothman@gmail.com>
2020-10-14 15:03:41 +1100

    Update deepweeds path; genericise rds acces

--- search/scripts/download_test_thumbnails.sh
+++ search/scripts/download_test_thumbnails.sh
@@ -5,14 +5,14 @@ mkdir -p "$thumbnails_root"
 tmpd=$(mktemp -d)
 trap 'rm -rf "$tmpd"; rm -rf "$thumbnails_root"/Makefile' EXIT
 
-rds_cp='scp research-data-int.sydney.edu.au:/rds/PRJ-iweeds'
+rds_cp='$rds_cp'
 
 cat > "$thumbnails_root"/Makefile <<EOF
 all: deepweeds cwfid digifarm-mungbeans artificial natural ginger
 
 deepweeds:
-	wget -O $tmpd/deepweeds.zip https://drive.google.com/u/0/uc?export=download&confirm=C_v5&id=1xnK3B6K6KekDI55vwJ0vnc2IGoDga9cj
 	mkdir $tmpd/deepweeds
+	$rds_cp/external_datasets/raw/deepweeds/images.zip $tmpd/deepweeds.zip
 	unzip -d $tmpd/deepweeds $tmpd/deepweeds.zip
 	rm -r $tmpd/deepweeds.zip
 	# find $tmpd/deepweeds -name "*.jpg" -exec magick "{}" -resize 300x300 '{}' ';'
@@ -28,26 +28,26 @@ cwfid:
 
 digifarm-mungbeans:
 	mkdir $tmpd/digifarm-mungbeans
-	scp research-data-int.sydney.edu.au:/rds/PRJ-iweeds/data/raw/SOLES/narrabri/2020-summer/20200402/img/*.jpg $tmpd/digifarm-mungbeans
+	$rds_cp/data/raw/SOLES/narrabri/2020-summer/20200402/img/*.jpg $tmpd/digifarm-mungbeans
 	find $tmpd/digifarm-mungbeans/ -name "*.jpg" -exec magick "{}" -resize 300x300 '{}' ';'
 	mv $tmpd/digifarm-mungbeans .
 
 
 artificial:
 	mkdir $tmpd/artificial
-	scp research-data-int.sydney.edu.au:/rds/PRJ-iweeds/data/raw/SOLES/narrabri/2019-winter/20190728/img/*.jpg $tmpd/artificial
+	$rds_cp/data/raw/SOLES/narrabri/2019-winter/20190728/img/*.jpg $tmpd/artificial
 	find $tmpd/artificial/ -name "*.jpg" -exec magick "{}" -resize 300x300 '{}' ';'
 	mv $tmpd/artificial .
 
 natural:
 	mkdir $tmpd/natural
-	scp research-data-int.sydney.edu.au:/rds/PRJ-iweeds/data/raw/SOLES/narrabri/2019-winter/20190729/img/*.jpg $tmpd/natural
+	$rds_cp/data/raw/SOLES/narrabri/2019-winter/20190729/img/*.jpg $tmpd/natural
 	find $tmpd/natural/ -name "*.jpg" -exec magick "{}" -resize 300x300 '{}' ';'
 	mv $tmpd/natural .
 
 ginger:
 	mkdir $tmpd/ginger
-	scp research-data-int.sydney.edu.au:/rds/PRJ-iweeds/data/raw/SOLES/chromakey/2020-spring/20200804/20200804-ginger-shed/img/*.png $tmpd/ginger
+	$rds_cp/data/raw/SOLES/chromakey/2020-spring/20200804/20200804-ginger-shed/img/*.png $tmpd/ginger
 	find $tmpd/ginger/ -name "*.png" -exec magick "{}" -resize 300x300 '{}' ';'
 	mv $tmpd/ginger .
 EOF
