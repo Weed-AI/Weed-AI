028bee64c5b729f9d3a082b505e0ba27ba8251c5
Elevn Li <zheng.li2@sydney.edu.au>
2020-10-21 16:56:16 +1100

    adding django and postgresql to docker-compose and nginx

--- search/controller/controller/settings.py
+++ search/controller/controller/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "sp5yk-!1ckot-kb!e23lqiw45wg4yv#7!z@p-*6hv!2bfk4mx_"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["controller"]
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "controller.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "reactivesearch",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
