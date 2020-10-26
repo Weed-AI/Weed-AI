6ea0904a7148f932dec31789a77532c46816c049
Elevn Li <zheng.li2@sydney.edu.au>
2020-10-26 13:20:10 +1100

    key vault using .env

--- search/controller/controller/settings.py
+++ search/controller/controller/settings.py
@@ -20,10 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_PATH = os.environ["SECRET_DJANGO_FILE"]
-print(SECRET_PATH)
-with open(SECRET_PATH) as f:
-    SECRET_KEY = f.readline()
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
