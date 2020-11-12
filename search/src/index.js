d66313b62cb815ecb5ce57b76362b4b3847b58f8
Henry Lydecker <henry.lydecker@gmail.com>
2020-11-12 13:45:16 +1100

    intital react json schema form
    autogenerated form from json schema

--- search/src/index.js
+++ search/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Form from './Form';
 import * as serviceWorker from './serviceWorker';
 
+//import RouterMapping from './RouterMapping';
+const RouterMapping = () => (
+  <Router>
+    <Route exact path='/' component={App} />
+    <Route path='/Form,' component={Form} />
+
+  </Router>
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterMapping />, 
   document.getElementById('root')
 );
 
