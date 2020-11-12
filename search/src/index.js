3f55295d1b2f06dc282498b76bfe309f86a9c080
Henry Lydecker <henry.lydecker@gmail.com>
2020-11-13 08:36:03 +1100

    Split form app
    Super basic react-jsonschema-form

--- search/src/index.js
+++ search/src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Form from './Form';
 import * as serviceWorker from './serviceWorker';
 
 //import RouterMapping from './RouterMapping';
 const RouterMapping = () => (
   <Router>
     <Route exact path='/' component={App} />
-    <Route path='/Form,' component={Form} />
 
   </Router>
 );
