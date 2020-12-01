073faf44b421adbbaddfaa11796e44b375d9ef8c
Elevn Li <51065213+ElevnLi@users.noreply.github.com>
2020-12-01 23:05:50 +1100

    Add deposit/repository functionality (#137)
    - Functionality to deposit weedcoco into on-disk repository
    - Functionality to create elasticsearch index and thumbnails from repository
    
    Co-authored-by: Joel Nothman <joel.nothman@sydney.edu.au>

---
+++ weedcoco/repo/hash_analyzer.py
@@ -0,0 +1,32 @@
+import numpy as np
+import imagehash
+import argparse
+import pathlib
+import os
+import scipy.spatial
+import PIL.Image
+import seaborn as sns
+import matplotlib.pyplot as plt
+from weedcoco.utils import check_if_approved_image_extension
+
+
+sns.set_theme(color_codes=True)
+
+ap = argparse.ArgumentParser(description=__doc__)
+ap.add_argument("--dataset-dir", required=True, type=pathlib.Path)
+args = ap.parse_args()
+
+X = np.asarray(
+    [
+        imagehash.average_hash(
+            PIL.Image.open(f"{dirpath}/{filename}"), hash_size=16
+        ).hash.flatten()
+        for dirpath, dirnames, filenames in os.walk(args.dataset_dir)
+        if os.path.basename(dirpath) == "img"
+        for filename in filenames
+        if check_if_approved_image_extension(filename)
+    ]
+)
+distances = scipy.spatial.distance.pdist(X, metric="hamming")
+g = sns.displot(distances)
+plt.show()
