9df358a4175717fa70f0384c0154b199672eeddd
Joel Nothman <joel.nothman@gmail.com>
2021-02-01 13:43:10 +1100

    Fix missing import of withRouter

--- search/src/App.js
+++ search/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route, Switch, Redirect } from "react-router-dom"
+import { Route, Switch, Redirect, withRouter } from "react-router-dom"
 import {ThemeProvider} from '@material-ui/styles'
 import NavbarComponent from './Components/wrapper/navbar'
 import theme from './Components/ui/theme'
