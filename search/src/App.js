a2059823f86c0a5e84c992994c7a54bbff8d86aa
Joel Nothman <joel.nothman@gmail.com>
2021-02-17 10:45:38 +1100

    I think it's better with the AgContext editor at /editor

--- search/src/App.js
+++ search/src/App.js
@@ -13,8 +13,8 @@ class App extends Component {
 			<ThemeProvider theme={theme}>
 				<Switch>
 					<Redirect exact from="/" to="/explore" />
-					<Route exact path='/agcontext/editor' component={AgContextEditor} />
-					<Route exact path='/metadata/editor' component={MetadataEditor} /> {/* XXX: for development */}
+					<Route exact path='/editor' component={AgContextEditor} />
+					<Route exact path='/meta-editor' component={MetadataEditor} /> {/* XXX: for development */}
 					<Route exact path="/:page/:dataset_id?" render={props => <NavbarComponent {...props} />} />
 				</Switch>
 			</ThemeProvider>
