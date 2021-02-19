6cab6c9f6e1c60c6df1bbc3005017afcc0115966
Joel Nothman <joel.nothman@gmail.com>
2021-02-19 16:49:50 +1100

    Handle bbch_growth_range == na

--- search/src/Components/dataset/dataset_summary.js
+++ search/src/Components/dataset/dataset_summary.js
@@ -52,7 +52,7 @@ const baseURL = new URL(window.location.origin);
 const AgContextFieldList = (props) => {
     const {title, agcontext, fields, classes, ...accordionProps} = props;
     const formatters = {
-      bbch_growth_range: (val) => (val["min"] + " to " + val["max"]),
+      bbch_growth_range: (val) => (val["min"] ? val["min"] + " to " + val["max"] : val),
     }
     const format = (key, val) => (formatters.hasOwnProperty(key) ? formatters[key](val) : val);
     return (
