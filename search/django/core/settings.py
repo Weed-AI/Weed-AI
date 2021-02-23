0be0d3648d549e3045d3aeedd23555b16ecab8ee
Joel Nothman <joel.nothman@gmail.com>
2021-02-23 16:30:25 +1100

    Fix line lenght

--- search/django/core/settings.py
+++ search/django/core/settings.py
@@ -24,7 +24,8 @@ AUTH_USER_MODEL = "weedid.WeedidUser"
 # Scale file size of upload limit up to 10 MB
 MAX_IMAGE_SIZE = 10485760
 DATA_UPLOAD_MAX_MEMORY_SIZE = MAX_IMAGE_SIZE
-FILE_UPLOAD_MAX_MEMORY_SIZE = MAX_IMAGE_SIZE   # see https://github.com/django-cms/django-filer/issues/1031
+# Avoid permissions bug, see https://github.com/django-cms/django-filer/issues/1031
+FILE_UPLOAD_MAX_MEMORY_SIZE = MAX_IMAGE_SIZE
 
 # Application definition
 
