e05161a9a7fc786d3d7535437a6f84ea100ce745
Joel Nothman <joel.nothman@gmail.com>
2021-03-17 22:12:37 +1100

    Serve sitemap.xml from Django
    Fixes #255

--- search/django/core/settings.py
+++ search/django/core/settings.py
@@ -1,5 +1,7 @@
 import os
 
+SITE_BASE_URL = "https://weed-ai.sydney.edu.au"
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 UPLOAD_DIR = os.path.join(BASE_DIR, "upload")
