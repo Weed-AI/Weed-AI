1401966f58896d59096a2e1794a87cc40c5635d6
Joel Nothman <joel.nothman@gmail.com>
2021-04-01 12:06:38 +1100

    Update SearchBase.js

--- search/src/Components/search/SearchBase.js
+++ search/src/Components/search/SearchBase.js
@@ -20,6 +20,7 @@ const theme = {
 const SearchBase = ({ children, baseURL }) => {
   return <ReactiveBase
     app="weedid"
+    mapKey="AIzaSyDDiJ4QoRW9_DJEV94ehO3z8zfCHRuHfxk"
     url={baseURL || new URL(window.location.origin) + "elasticsearch/"}
     theme={theme}
     headers={{'X-CSRFToken': Cookies.get('csrftoken')}}
