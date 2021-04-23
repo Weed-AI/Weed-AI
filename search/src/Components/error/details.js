9c586e45085c58ad922392a27d16bbc82d3cd47b
Elevn Li <zheng.li411@gmail.com>
2021-04-23 12:57:38 +1000

    fixes

--- search/src/Components/error/details.js
+++ search/src/Components/error/details.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {jsonSchemaTransform} from './utils';
+import ReactMarkdown from "react-markdown";
 
 
 const JsonSchemaDetails = (props) => {
@@ -9,9 +10,13 @@ const JsonSchemaDetails = (props) => {
         return (
             <div>
                 <p>{tfError[path].instances.length} {tfError[path].instances.length > 1 ? "errors": "error"} in the {path}</p>
-                {tfError[path].instances.map((error) => <p>{error.message} @ {error.path}</p>)}
-                <p>Description:</p>
-                <p>{tfError[path].description}</p>
+                <ol>
+                    {tfError[path].instances.map(error =>
+                        <li key={error.path}>In {error.path}: {error.message}</li>
+                    )}
+                </ol>
+                {tfError[path].description ? <p>Description of this field:</p> : ""}
+                <ReactMarkdown source={tfError[path].description} />
                 <br/>
             </div>
         )
--- search/src/Components/error/utils.js
+++ search/src/Components/error/utils.js
@@ -4,14 +4,20 @@ export const jsonSchemaTitle = (res) => {
 
 export const jsonSchemaTransform = (details) => {
     const transform = {}
-    const pathKey = (path) => `'${path[2]}' field of ${path[0]}`
+    const pathKey = (path) => path.length === 1 ?
+                              path[0] :
+                              path.length === 2 ?
+                              `'${path[1]}' field of ${path[0]}` :
+                              path.length === 3 ?
+                              `'${path[2]}' field of ${path[1]} in ${path[0]}` :
+                              path.join(".")
     for (const error_detail of details.error_details) {
-        let path = pathKey(error_detail.path)
-        if (path in transform) {
-            transform[path].instances.push({path: error_detail.path.join(" / "), message: error_detail.message})
-        } else {
-            transform[path] = {description: error_detail.schema.description, instances: [{path: error_detail.path.join(" / "), message: error_detail.message}]}
+        let namedPaths = error_detail.path.filter(word => typeof word == typeof '')
+        let path = pathKey(namedPaths)
+        if (!(path in transform)) {
+            transform[path] = {description: error_detail.schema.description, instances: []}
         }
+        transform[path].instances.push({path: error_detail.path.join("."), message: error_detail.message});
     }
     return transform
 }
