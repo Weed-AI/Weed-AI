c24f617c7257db29c02240992bb27a700efeefad
Elevn Li <zheng.li411@gmail.com>
2021-05-05 16:29:48 +1000

    change email format

--- search/django/weedid/notification.py
+++ search/django/weedid/notification.py
@@ -8,9 +8,8 @@ def upload_notification(upload_id):
     upload_entity = Dataset.objects.get(upload_id=upload_id)
     uploader = upload_entity.user
     email_body = f"""\
-    User {uploader.username} <{uploader.email}> has uploaded a dataset {upload_entity.metadata['name']} to review.
-    {'You can check it from ' + SITE_BASE_URL + '/datasets/' + upload_id}.
-
+User {uploader.username} <{uploader.email}> has uploaded a dataset {upload_entity.metadata['name']} to review.
+{'You can check it from ' + SITE_BASE_URL + '/datasets/' + upload_id}.
     """
     staff_recipients = [
         staff.email for staff in WeedidUser.objects.filter(is_staff=True)
@@ -26,11 +25,13 @@ def review_notification(message, upload_id):
     upload_entity = Dataset.objects.get(upload_id=upload_id)
     uploader = upload_entity.user
     email_body = f"""\
-    Dear Weed-AI contributor,
+Dear Weed-AI contributor,
 
-    Your dataset upload {upload_entity.metadata['name']} has been {message} after review
-    {'You can check it from ' + SITE_BASE_URL + '/datasets/' + upload_id if message == 'approved' else 'Feel free to contact our admin'}.
+Your dataset upload {upload_entity.metadata['name']} has been {message} after review
+{'You can check it from ' + SITE_BASE_URL + '/datasets/' + upload_id if message == 'approved' else 'Feel free to contact our admin'}.
 
+Regards,
+WeedAI Team
     """
     send_email(
         f"Your upload ({upload_entity.metadata['name']}) has been {message}",
