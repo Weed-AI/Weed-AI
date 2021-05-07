fba6437eb22d41e202178b0e9f98d034a0368595
Elevn Li <zheng.li411@gmail.com>
2021-05-07 11:45:10 +1000

    add testing-library to commands

--- search/cypress/support/commands.js
+++ search/cypress/support/commands.js
@@ -24,3 +24,4 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 import 'cypress-file-upload';
+import '@testing-library/cypress/add-commands';
