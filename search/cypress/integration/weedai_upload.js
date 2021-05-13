3e966223737a6df2dda0eb7ff7df9854192d5a70
Elevn Li <zheng.li411@gmail.com>
2021-05-13 11:05:42 +1000

    add test registration and login

--- search/cypress/integration/weedai_upload.js
+++ search/cypress/integration/weedai_upload.js
@@ -2,13 +2,30 @@ describe('Test the overallworkflow', () => {
 
     beforeEach(() => {
         cy.visit('http://localhost/upload', {failOnStatusCode: false})
+
+        const test_username = 'admin_test'
+        const test_password = 'password_test'
+        const test_email = 'admin_test@weed-ai.com'
+
+        cy.findAllByText(/^Sign up$/).should('have.length', 1).click()
+        cy.get('#username')
+          .type(test_username)
+          .should('have.value', test_username)
+        cy.get('#password')
+          .type(test_password)
+          .should('have.value', test_password)
+        cy.get('#email')
+          .type(test_email)
+          .should('have.value', test_email)
+        cy.findAllByRole('button', /^Sign Up$/).should('have.length', 1).click()
+
         cy.findAllByText(/^Sign In$/).should('have.length', 1).click()
         cy.get('#username')
-          .type('admin')
-          .should('have.value', 'admin')
+          .type(test_username)
+          .should('have.value', test_username)
         cy.get('#password')
-          .type('password')
-          .should('have.value', 'password')
+          .type(test_password)
+          .should('have.value', test_password)
         cy.findAllByRole('button', /^Sign In$/).should('have.length', 1).click()
     })
 
@@ -21,9 +38,9 @@ describe('Test the overallworkflow', () => {
         cy.findAllByText(/^Submit$/).should('have.length', 1).click()
     })
 
-    it('Test Coco Upload', () => {
+    // it('Test Coco Upload', () => {
 
-    })
+    // })
 
     afterEach(() => {
         cy.findAllByText(/^Log out$/).should('have.length', 1).click()
