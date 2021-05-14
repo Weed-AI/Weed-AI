a5b69c469a27ac79fc3ce18c82d6adae03c6df96
Elevn Li <51065213+ElevnLi@users.noreply.github.com>
2021-05-14 11:21:33 +1000

    E2e test with cypress (#447)

---
+++ search/cypress/support/commands.js
@@ -0,0 +1,52 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import 'cypress-file-upload';
+import '@testing-library/cypress/add-commands';
+
+Cypress.Commands.add('register', (username, email, password) => {
+    cy.findAllByText(/^Sign up$/).should('have.length', 1).click()
+    cy.get('#username')
+    .type(username)
+    .should('have.value', username)
+    cy.get('#email')
+    .type(email)
+    .should('have.value', email)
+    cy.get('#password')
+    .type(password)
+    .should('have.value', password)
+    cy.findAllByRole('button', /^Sign Up$/).should('have.length', 1).click()
+})
+
+Cypress.Commands.add('login', (username, password) => {
+    cy.findAllByText(/^Sign In$/).should('have.length', 1).click()
+    cy.get('#username')
+    .type(username)
+    .should('have.value', username)
+    cy.get('#password')
+    .type(password)
+    .should('have.value', password)
+    cy.findAllByRole('button', /^Sign In$/).should('have.length', 1).click()
+})
