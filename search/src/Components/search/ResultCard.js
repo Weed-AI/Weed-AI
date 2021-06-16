bf06df01d3dca687a45e9855ef3ddd0bd59670a5
Joel Nothman <joel.nothman@gmail.com>
2021-06-16 11:43:12 +1000

    WIP
    Magnifier layout isn't great, needs to export and use thumbnail_large instead of full-size image server.

--- search/src/Components/search/ResultCard.js
+++ search/src/Components/search/ResultCard.js
@@ -3,9 +3,64 @@ import Tooltip from '@material-ui/core/Tooltip';
 import {
     ResultCard
 } from '@appbaseio/reactivesearch';
+import { makeStyles } from '@material-ui/core/styles';
+import Magnifier from "react-magnifier";
+import {
+  MagnifierContainer, MagnifierZoom, MagnifierPreview,
+} from "react-image-magnifiers";
+
+const useStyles = makeStyles(theme => (console.log(theme) || {
+  imageContainer: {
+    width: "calc(100% + 20px)",
+    maxHeight: "220px",
+    margin: "0 0 0 -10px",
+    position: "relative",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    maxWidth: "100%",
+    maxHeight: "100%",
+    width: "auto",
+    height: "auto",
+  },
+}));
+
+
+const BBox = ({ item, annot }) => {
+  const containerHeight = 220;
+  const containerWidth = 250; // XXX: can we compute this? it can be less...
+  const aspectRatio = item.width / item.height;
+  const thumbScale = aspectRatio >= (containerWidth / containerHeight) ? containerWidth / item.width : containerHeight / item.height;
+  const thumbTop = 0 // (containerHeight - item.height * thumbScale) / 2;
+  const thumbLeft = 0 // (containerWidth - item.width * thumbScale) / 2;
+  const species = annot.category.name.match(/^([^:]*): (.*)/)[2]
+  const color = annot.category.name.startsWith("weed") ? "#dc3545" : annot.category.name.startsWith("crop") ? "#28a745" : "#808080";
+  return <div style={{
+      pointerEvents: "none",
+      border: "1px solid",
+      borderColor: color,
+      position: "absolute",
+      left: thumbLeft + thumbScale * annot.bbox[0],
+      top: thumbTop + thumbScale * annot.bbox[1],
+      width: thumbScale * annot.bbox[2],
+      height: thumbScale * annot.bbox[3],
+  }} >
+    { species ? <span style={{
+      fontSize: ".5em",
+      background: color + "30",  // transparent
+      color: "white",
+      position: "absolute",
+      overflow: "hidden",
+      maxWidth: Math.max(60, thumbScale * annot.bbox[2]),
+      top: 0,
+    }}>{species}</span> : [] }
+  </div>
+}
 
 const WeedAIResultCard = (props) => {
   const {item, baseURL, linkToDataset} = props;
+  const classes = useStyles();
   const formatCropType = (typ) => {
     return typ === "other" ? "other crop type" : typ;
   }
@@ -18,11 +73,15 @@ const WeedAIResultCard = (props) => {
     taskTypes.includes("segmentation") ? "segment" : (taskTypes.includes("bounding box") ? "bounding box" : "labels"));
   const pluralise = (text, n) => n === 1 ? text : (text.match(/[zsx]$/) ? text + "es" : text + "s");
   return (
-    <ResultCard>
-        <ResultCard.Image
-            src={item.thumbnail_bbox || item.thumbnail}
-        />
-        <ResultCard.Description>
+    <ResultCard style={{position: "relative"}}>
+        <div className={classes.imageContainer}>
+    <MagnifierContainer >
+    <MagnifierPreview imageSrc={item.thumbnail} className={classes.image} />
+            {item.annotations.filter(annot => annot.bbox).map(annot => <BBox item={item} annot={annot} />)}
+    <MagnifierZoom style={{ height: "120px", zIndex: 5 }} imageSrc={"/code/images/" + item.upload_id + "/" + item.file_name} />
+</MagnifierContainer>
+        </div>
+        <ResultCard.Description style={{position: "absolute", bottom: "12px"}}>
             <ul className="annotations">
             {
                 // TODO: make this more idiomatically React
@@ -42,7 +101,7 @@ const WeedAIResultCard = (props) => {
                 : "") +
               "."
             }
-            {linkToDataset === false ? [] : <div><Tooltip title={item.dataset_name}><a title={item.dataset_name} href={`${baseURL}datasets/${item.upload_id}`}>See Dataset</a></Tooltip></div>}
+            {linkToDataset === false ? [] : <div><Tooltip title={item.dataset_name}><a href={`${baseURL}datasets/${item.upload_id}`}>See Dataset</a></Tooltip></div>}
         </ResultCard.Description>
     </ResultCard>
   );
