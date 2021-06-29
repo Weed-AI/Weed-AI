91e4ad67ef9b755614f61553426de5fc3efb62de
Joel Nothman <joel.nothman@gmail.com>
2021-06-29 16:19:10 +1000

    Fix settings for beat

--- search/django/core/settings.py
+++ search/django/core/settings.py
@@ -147,11 +147,11 @@ GIT_REMOTE_PATH = os.environ.get("GIT_REMOTE_PATH")
 DVC_REMOTE_PATH = os.environ.get("GIT_REMOTE_PATH")
 
 if GIT_REMOTE_PATH and DVC_REMOTE_PATH:
-    CELERY_beat_schedule = {
+    CELERY_BEAT_SCHEDULE = {
         "add-at-melbourne-sunset": {
             "task": "weedid.tasks.backup_repository_changes",
             # "schedule": crontab(minute=0, hour="*/3"),
-            "schedule": crontab(minute="*", hour="*/3"),
+            "schedule": crontab(minute="*/5", hour="*"),
         },
     }
 elif not DEBUG:
