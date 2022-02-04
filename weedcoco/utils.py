be80dda15cbe5618bddea93cfa4c0e7c3c5d3bce
Mike Lynch <mlynch@sydney.edu.au>
2022-02-04 17:00:57 +1100

    Flake8 lint fixes

--- weedcoco/utils.py
+++ weedcoco/utils.py
@@ -193,7 +193,5 @@ def denormalise_weedcoco(weedcoco):
 def fix_compatibility_quirks(weedcoco):
     """Fix minor issues for compatibility with other COCO tools. Operates in-place"""
     for ann in weedcoco["annotations"]:
-        if "bbox" in ann and not "segmentation" in ann:
+        if "bbox" in ann and "segmentation" not in ann:
             ann["segmentation"] = []
-
-
