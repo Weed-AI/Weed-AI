6bb13ec68e230a5d8de7691c683437f06a095e59
Joel Nothman <joel.nothman@gmail.com>
2022-02-23 18:05:12 +1100

    Fix an issue in GBIF lookup of category

--- search/src/Common/weedcocoUtil.js
+++ search/src/Common/weedcocoUtil.js
@@ -1,6 +1,7 @@
 export const parseCategoryName = (categoryName) => {
   const groups = categoryName.match(/^([^:]*): (.*)/);
-  return groups === null ? {role: categoryName} : {role: groups[1], species: groups[2]};
+  console.log('*', groups)
+  return groups === null ? {role: categoryName} : {role: groups[1], taxon: groups[2]};
 }
 
 
