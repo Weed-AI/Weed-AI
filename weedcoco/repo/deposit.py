0f1cbac663e2bacb4545eff5bb5c86d0d9fecbf1
Mike Lynch <mlynch@sydney.edu.au>
2022-03-31 16:51:13 +1100

    Replacing the exif manipulation stuff with a straight copy to test my theory about ocfl duplicates

--- weedcoco/repo/deposit.py
+++ weedcoco/repo/deposit.py
@@ -12,7 +12,8 @@ from zipfile import ZipFile
 
 import ocfl
 import redis
-from PIL import Image
+
+# from PIL import Image
 from weedcoco.utils import check_if_approved_image_extension, get_image_hash
 from weedcoco.validation import ValidationError, validate
 
@@ -209,12 +210,13 @@ class RepositoryDataset:
                     )
                     image_path = new_image_dir / self.image_hash[image_name_origin]
                     if not os.path.isfile(image_path):
-                        image_origin = Image.open(self.image_dir / image_name_origin)
-                        image_without_exif = Image.new(
-                            image_origin.mode, image_origin.size
-                        )
-                        image_without_exif.putdata(image_origin.getdata())
-                        image_without_exif.save(image_path)
+                        copy(self.image_dir / image_name_origin, image_path)
+                        # image_origin = Image.open(self.image_dir / image_name_origin)
+                        # image_without_exif = Image.new(
+                        #     image_origin.mode, image_origin.size
+                        # )
+                        # image_without_exif.putdata(image_origin.getdata())
+                        # image_without_exif.save(image_path)
 
     def validate_duplicate_images(self):
         if len(get_hashset_from_image_name(self.image_hash)) != len(self.image_hash):
